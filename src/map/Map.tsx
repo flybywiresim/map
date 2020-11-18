01391bac051b4705d9f13136cf88342124030c17
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-11-18 13:21:05 -0800

    Initial Commit

---
+++ src/map/Map.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+type MapProps = {}
+
+type MapState = {}
+
+export default class Map extends React.Component<MapProps, MapState> {
+    render() {
+        return (
+            <div>
+                <p>Map</p>
+            </div>
+        );
+    }
+}
