20a8d911910448fc6a3f0574bf4949d3cd581a5d
ZigTag <17077422+ZigTag@users.noreply.github.com>
2020-11-20 13:54:02 -0800

    Basic Map Displaying

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Map from './map/Map';
-
 import './App.scss';
 
 function App() {
--- src/map/Map.tsx
+++ src/map/Map.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import {TileLayer, MapContainer} from "react-leaflet";
+
+import "leaflet/dist/leaflet.css";
+import "./Map.scss";
 
 type MapProps = {}
 
@@ -8,7 +12,16 @@ export default class Map extends React.Component<MapProps, MapState> {
     render() {
         return (
             <div>
-                <p>Map</p>
+                <MapContainer
+                    id="mapid"
+                    center={[51.505, -0.09]}
+                    zoom={13}
+                    scrollWheelZoom={true}>
+                    <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                    />
+                </MapContainer>
             </div>
         );
     }
