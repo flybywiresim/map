0a638002009b31823ed7cf1ec31604911194bd65
Niklas Steiner <niklas@sbg.at>
2021-01-07 16:11:57 +0100

    Fix map scaling

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import './App.scss';
 
 function App() {
     return (
-        <div className="App">
+        <div className="App" style={{height: "100vh", width: "100vw"}}>
             <Map />
         </div>
     );
